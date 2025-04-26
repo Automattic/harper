3184ffe4cc847e15900ba140ac278d8ce7bf493b
Elijah Potter <me@elijahpotter.dev>
2025-04-26 10:32:11 -0600

    fix(core): appease clippy

--- harper-core/src/patterns/inflection_of_be.rs
+++ harper-core/src/patterns/inflection_of_be.rs
@@ -5,11 +5,17 @@ use crate::patterns::WordSet;
 /// Matches any inflection of the verb “be”:
 /// `am`, `is`, `are`, `was`, `were`, `be`, `been`, `being`.
 pub struct InflectionOfBe {
+    /// If using a `WordSet` proves expensive, we'll switch to something else.
     inner: WordSet,
 }
 
+impl Default for InflectionOfBe {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl InflectionOfBe {
-    /// Create a new `BeInflection` matcher.
     pub fn new() -> Self {
         Self {
             inner: WordSet::new(&["be", "am", "is", "are", "was", "were", "been", "being"]),
