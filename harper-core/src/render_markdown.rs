3c9b31cbbb7b7fdaacc80172ef418532dc11c77b
Elijah Potter <me@elijahpotter.dev>
2025-05-07 13:11:59 -0600

    fix(core): sanitize untrusted input

--- harper-core/src/render_markdown.rs
+++ harper-core/src/render_markdown.rs
@@ -1,8 +1,11 @@
+use ammonia::clean;
+use pulldown_cmark::{Parser, html};
+
 /// The standard Markdown rendering function for the crate.
 /// Do not call `pulldown_cmark` directly. Use this.
 pub fn render_markdown(markdown: &str) -> String {
-    let parser = pulldown_cmark::Parser::new(markdown);
+    let parser = Parser::new(markdown);
     let mut html = String::new();
-    pulldown_cmark::html::push_html(&mut html, parser);
-    html
+    html::push_html(&mut html, parser);
+    clean(&html)
 }
