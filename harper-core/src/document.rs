20b6cf304a4c41d4adbb2a143f51b039f5bc9ec6
Andrew Hennis <dawnandrew100@gmail.com>
2025-05-19 19:02:04 -0400

    Added determiners to function
    Added a list of determiners from the 2003 edition of "Longman Student
    Grammar of Spoken and Written English". This list is non-exhaustive and
    only includes those determiners on page 65 listed as determiners for
    countable or uncountable nouns

--- harper-core/src/document.rs
+++ harper-core/src/document.rs
@@ -224,6 +224,11 @@ impl Document {
                         "my", "your", "thy", "thine", "his", /*"her",*/ "its", "our", "their",
                         "whose", // "no" is also a determiner
                         "no",
+                        // List of Determiners from the 2003 "Longman Student Grammar of
+                        // Spoken and Written English" Chapter 4.5
+                        "all", "both", "half", "this", "that", "every", "each", "either", "neither",
+                        "any", "many", "some", "these", "those", "few", "enough", "several",
+                        "much",
                     ]))
                     .then_whitespace()
                     .then_verb(),
