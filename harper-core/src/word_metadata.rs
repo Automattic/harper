fafc1ba96e92b2d8a09b5f7082a3b288ff2d6d08
86xsk <200443667+86xsk@users.noreply.github.com>
2025-06-22 17:53:56 -0500

    feat(core): add dialect detection functionality

--- harper-core/src/word_metadata.rs
+++ harper-core/src/word_metadata.rs
@@ -1,12 +1,14 @@
 use harper_brill::UPOS;
+use hashbrown::HashMap;
 use is_macro::Is;
 use paste::paste;
 use serde::{Deserialize, Serialize};
-use strum_macros::{Display, EnumString};
+use strum::VariantArray;
+use strum_macros::{Display, EnumString, VariantArray};
 
 use std::convert::TryFrom;
 
-use crate::WordId;
+use crate::{Document, TokenKind, TokenStringExt, WordId};
 
 #[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize, PartialOrd, Hash)]
 pub struct WordMetadata {
@@ -603,7 +605,18 @@ impl ConjunctionData {
 
 /// A regional dialect.
 #[derive(
-    Debug, Clone, Copy, Serialize, Deserialize, PartialEq, PartialOrd, Eq, Hash, EnumString, Display,
+    Debug,
+    Clone,
+    Copy,
+    Serialize,
+    Deserialize,
+    PartialEq,
+    PartialOrd,
+    Eq,
+    Hash,
+    EnumString,
+    Display,
+    VariantArray,
 )]
 pub enum Dialect {
     // Note: these have bit-shifted values so that they can ergonomically integrate with
@@ -613,6 +626,14 @@ pub enum Dialect {
     Canadian = 1 << 1,
     Australian = 1 << 2,
     British = 1 << 3,
+}
+impl Dialect {
+    /// Tries to guess the dialect used in the document by finding which dialect is used the most.
+    /// Returns `None` if it fails to find a single dialect that is used the most.
+    #[must_use]
+    pub fn try_guess_from_document(document: &Document) -> Option<Self> {
+        Self::try_from(DialectFlags::get_most_used_dialects_from_document(document)).ok()
+    }
 }
 impl TryFrom<DialectFlags> for Dialect {
     type Error = ();
@@ -688,6 +709,42 @@ impl DialectFlags {
             panic!("The '{dialect}' dialect isn't defined in DialectFlags!");
         };
         out
+    }
+
+    /// Gets the most commonly used dialect(s) in the document.
+    ///
+    /// If multiple dialects are used equally often, they will all be enabled in the returned
+    /// `DialectFlags`. On the other hand, if there is a single dialect that is used the most, it
+    /// will be the only one enabled.
+    #[must_use]
+    pub fn get_most_used_dialects_from_document(document: &Document) -> Self {
+        // Initialize counters.
+        let mut dialect_counters: HashMap<Dialect, usize> =
+            HashMap::from_iter(Dialect::VARIANTS.iter().map(|d| (*d, 0)));
+
+        // Count word dialects.
+        document.iter_words().for_each(|w| {
+            if let TokenKind::Word(Some(word_metadata)) = &w.kind {
+                // If the token is a word, iterate though the dialects in `dialect_counters` and
+                // increment those counters where the word has the respective dialect enabled.
+                dialect_counters.iter_mut().for_each(|(dialect, acc)| {
+                    if word_metadata.dialects.is_dialect_enabled(*dialect) {
+                        *acc += 1;
+                    }
+                });
+            }
+        });
+
+        // Find max counter.
+        let max_counter = dialect_counters.values().max().unwrap();
+        // Get and convert the collection of most used dialects into a `DialectFlags`.
+        dialect_counters
+            .iter()
+            .filter(|(_, count)| **count == *max_counter)
+            .fold(DialectFlags::empty(), |acc, dialect| {
+                // Fold most used dialects into `DialectFlags` via bitwise or.
+                acc | Self::from_dialect(*dialect.0)
+            })
     }
 }
 impl Default for DialectFlags {
