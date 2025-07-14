1861706df2a8c00383972316ec33a621158c3aa1
thuan1412 <thuan14121999@gmail.com>
2025-07-14 20:37:54 +0700

    chore: add some exceptional cases

--- harper-core/src/linting/on_floor.rs
+++ harper-core/src/linting/on_floor.rs
@@ -1,9 +1,10 @@
 use crate::CharStringExt;
 use crate::expr::Expr;
+use crate::expr::LongestMatchOf;
 use crate::expr::SequenceExpr;
 use crate::patterns::WordSet;
 use crate::{
-    Token, TokenStringExt,
+    Lrc, Token, TokenStringExt,
     linting::{ExprLinter, Lint, LintKind, Suggestion},
 };
 
@@ -15,14 +16,39 @@ impl Default for OnFloor {
     fn default() -> Self {
         let preposition = WordSet::new(&["in", "at"]);
 
-        let pattern = SequenceExpr::default()
-            .then(preposition)
-            .t_ws()
-            .t_aco("the")
-            .t_ws()
-            .t_any()
-            .t_ws()
-            .t_aco("floor");
+        let on_the_floor = Lrc::new(
+            SequenceExpr::default()
+                .then(preposition)
+                .t_ws()
+                .t_aco("the")
+                .t_ws()
+                .t_any()
+                .t_ws()
+                .t_aco("floor"),
+        );
+
+        let look_up_phrase = Lrc::new(
+            SequenceExpr::default()
+                .then(WordSet::new(&["look", "looking", "looks", "looked"]))
+                .t_ws()
+                .t_aco("up"),
+        );
+
+        let stop = Lrc::new(WordSet::new(&["stop", "stopping", "stops", "stopped"]));
+        let exceptions = Lrc::new(LongestMatchOf::new(vec![
+            Box::new(SequenceExpr::default().then(look_up_phrase.clone())),
+            Box::new(SequenceExpr::default().then(stop.clone())),
+        ]));
+
+        let pattern = LongestMatchOf::new(vec![
+            Box::new(on_the_floor.clone()),
+            Box::new(
+                SequenceExpr::default()
+                    .then(exceptions.clone())
+                    .t_ws()
+                    .then(on_the_floor.clone()),
+            ),
+        ]);
 
         Self {
             expr: Box::new(pattern),
@@ -37,6 +63,11 @@ impl ExprLinter for OnFloor {
 
     fn match_to_lint(&self, matched_tokens: &[Token], source: &[char]) -> Option<Lint> {
         let incorrect_preposition = matched_tokens[0..1].span()?.get_content(source).to_string();
+        // if the first token is not "in" or "at", means that the match is belong to the exceptions
+        // so we don't need to lint it
+        if !["in", "at"].contains(&incorrect_preposition.to_lowercase().as_str()) {
+            return None;
+        }
         let span = matched_tokens[0..1].span()?;
 
         Some(Lint {
@@ -106,6 +137,29 @@ mod tests {
             "At the second floor of a building.",
             OnFloor::default(),
             "On the second floor of a building.",
+        );
+    }
+
+    #[test]
+    fn no_lint_with_look_up_at() {
+        assert_lint_count("She looked up at the third floor.", OnFloor::default(), 0);
+    }
+
+    #[test]
+    fn no_lint_with_stop_at() {
+        assert_lint_count(
+            "The elevator stops at the 3rd floor of a building.",
+            OnFloor::default(),
+            0,
+        );
+    }
+
+    #[test]
+    fn no_lint_with_looking_up_at() {
+        assert_lint_count(
+            "The workers are looking up at the 3rd floor of a building.",
+            OnFloor::default(),
+            0,
         );
     }
 }
