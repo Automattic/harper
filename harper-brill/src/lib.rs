6640c46737a66bc6ece7db82aedb02bc40ee84d0
Elijah Potter <me@elijahpotter.dev>
2025-07-28 11:23:34 -0600

    feat: add aggressive caching

--- harper-brill/src/lib.rs
+++ harper-brill/src/lib.rs
@@ -1,5 +1,6 @@
-use harper_pos_utils::BurnChunkerCpu;
+use harper_pos_utils::{BurnChunkerCpu, CachedChunker};
 use lazy_static::lazy_static;
+use std::num::NonZero;
 use std::rc::Rc;
 use std::sync::Arc;
 
@@ -37,13 +38,16 @@ const BURN_CHUNKER_VOCAB: &[u8; 529111] = include_bytes!("../finished_chunker/vo
 const BURN_CHUNKER_BIN: &[u8; 806312] = include_bytes!("../finished_chunker/model.mpk");
 
 thread_local! {
-    static BURN_CHUNKER: Rc<BurnChunkerCpu> =  Rc::new(uncached_burn_chunker()) ;
+    static BURN_CHUNKER: Rc<CachedChunker<BurnChunkerCpu>> =  Rc::new(uncached_burn_chunker()) ;
 }
 
-fn uncached_burn_chunker() -> BurnChunkerCpu {
-    BurnChunkerCpu::load_from_bytes_cpu(BURN_CHUNKER_BIN, BURN_CHUNKER_VOCAB, 6, 0.3)
+fn uncached_burn_chunker() -> CachedChunker<BurnChunkerCpu> {
+    CachedChunker::new(
+        BurnChunkerCpu::load_from_bytes_cpu(BURN_CHUNKER_BIN, BURN_CHUNKER_VOCAB, 6, 0.3),
+        NonZero::new(10000).unwrap(),
+    )
 }
 
-pub fn burn_chunker() -> Rc<BurnChunkerCpu> {
+pub fn burn_chunker() -> Rc<CachedChunker<BurnChunkerCpu>> {
     (BURN_CHUNKER).with(|c| c.clone())
 }
