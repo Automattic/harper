f06b46a47b01f83fa15f07ed5e951459c5899218
hippietrail <hippytrail@gmail.com>
2025-07-31 00:43:46 +0900

    feat: quality-of-life methods for `SequenceExpr`
    - `then_word_except`
    - `then_kind_except`
    - `then_kind_both`
    - `then_kind_either`
    - `then_kind_any`
    - `then_kind_is_but_is_not`
    - `then_kind_is_but_is_not_except`
    
    Refactor linters to use the new methods.
    Refactor other `TokenKind` predicate use in linters.
    Move `SpanVecExt` linting assertion helper to be accessible to all linters.

--- harper-core/src/linting/that_than.rs
+++ harper-core/src/linting/that_than.rs
@@ -1,5 +1,5 @@
 use crate::{
-    CharStringExt, Token,
+    Token, TokenKind,
     expr::{Expr, SequenceExpr},
     linting::{ExprLinter, Lint, LintKind, Suggestion},
 };
@@ -11,19 +11,11 @@ pub struct ThatThan {
 impl Default for ThatThan {
     fn default() -> Self {
         let adjective_er_that_nextword = SequenceExpr::default()
-            .then(|tok: &Token, src: &[char]| {
-                if !tok.kind.is_comparative_adjective() {
-                    return false;
-                }
-                let adj = tok.span.get_content(src);
-                !adj.eq_ignore_ascii_case_str("better") && !adj.eq_ignore_ascii_case_str("later")
-            })
+            .then_kind_except(TokenKind::is_comparative_adjective, &["better", "later"])
             .t_ws()
             .t_aco("that")
             .t_ws()
-            .then(|tok: &Token, src: &[char]| {
-                tok.kind.is_word() && !tok.span.get_content(src).eq_ignore_ascii_case_str("way")
-            });
+            .then_word_except(&["way"]);
 
         Self {
             expr: Box::new(adjective_er_that_nextword),
