e9092a3330e40e129463208e7c27c1cb720d0cba
hippietrail <hippytrail@gmail.com>
2025-08-22 18:42:45 +0900

    feat: would've never/would never have→never would have

--- harper-core/src/linting/mod.rs
+++ harper-core/src/linting/mod.rs
@@ -134,6 +134,7 @@ mod whereas;
 mod widely_accepted;
 mod win_prize;
 mod wordpress_dotcom;
+mod would_never_have;
 
 pub use a_part::APart;
 pub use adjective_double_degree::AdjectiveDoubleDegree;
@@ -253,6 +254,7 @@ pub use whereas::Whereas;
 pub use widely_accepted::WidelyAccepted;
 pub use win_prize::WinPrize;
 pub use wordpress_dotcom::WordPressDotcom;
+pub use would_never_have::WouldNeverHave;
 
 use crate::{Document, LSend, render_markdown};
 
