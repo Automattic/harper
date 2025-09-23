f5a8b268cc925791e97b254e028977803fb5b040
Elijah Potter <me@elijahpotter.dev>
2025-09-23 12:14:08 -0600

    fix(core): crash

--- harper-comments/src/comment_parser.rs
+++ harper-comments/src/comment_parser.rs
@@ -123,16 +123,3 @@ impl Parser for CommentParser {
         self.inner.parse(source)
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::CommentParser;
-    use harper_core::parsers::{MarkdownOptions, StrParser};
-
-    #[test]
-    fn hang() {
-        let opts = MarkdownOptions::default();
-        let parser = CommentParser::new_from_language_id("cmake", opts).unwrap();
-        let _res = parser.parse_str("#[[#|]]:A]");
-    }
-}
