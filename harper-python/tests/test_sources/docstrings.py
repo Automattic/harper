885599cad6e7036360728efe541eacb89663ef35
Artem Golubin <me@rushter.com>
2025-10-01 02:37:54 +0400

    feat(core): Add support for Python docstrings

---
+++ harper-python/tests/test_sources/docstrings.py
@@ -0,0 +1,11 @@
+"""Errors should never passs silently"""
+def main():
+    """Beautifull is better than ugly."""
+
+
+class Main:
+    """Explicit is better than implicet."""
+
+    def __init__(self):
+        """Flat is bettter than nested."""
+        pass
