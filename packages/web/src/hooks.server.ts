fdc14fcbf753ceaaf698ea7c6adabca550b6b9bd
Elijah Potter <me@elijahpotter.dev>
2025-10-22 10:23:45 -0600

    chore(web): `just format`

--- packages/web/src/hooks.server.ts
+++ packages/web/src/hooks.server.ts
@@ -3,8 +3,8 @@ import { migrate } from 'drizzle-orm/mysql2/migrator';
 
 // Migrate exactly once at startup
 try {
-  await migrate(db, { migrationsFolder: './drizzle', migrationsTable: '__drizzle_migrations' });
-} catch(e: any){
-  console.log("Failed to migrate database.");
-  console.error(e); 
+	await migrate(db, { migrationsFolder: './drizzle', migrationsTable: '__drizzle_migrations' });
+} catch (e: any) {
+	console.log('Failed to migrate database.');
+	console.error(e);
 }
