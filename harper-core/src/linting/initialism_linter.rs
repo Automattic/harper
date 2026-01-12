655de5481e24e086da5a4c020611c4401d855cbf
hippietrail <hippytrail@gmail.com>
2026-01-12 20:29:14 +0700

    refactor: replace for-loops with functional style code

--- harper-core/src/linting/initialism_linter.rs
+++ harper-core/src/linting/initialism_linter.rs
@@ -7,7 +7,7 @@ use crate::linting::expr_linter::Chunk;
 
 /// Alias for a word in an initialism expansion
 type InitialismWord = Vec<char>;
-/// Alias for a phrase in an initialism expansion
+/// Alias for a phrase an initialism expands to
 type InitialismPhrase = Vec<InitialismWord>;
 
 /// A struct that can be composed to expand initialisms, respecting the capitalization of each
@@ -21,15 +21,15 @@ pub struct InitialismLinter {
 impl InitialismLinter {
     /// Construct a linter that can correct an initialism to
     pub fn new(initialism: &str, expansions: &[&str]) -> Self {
-        let mut expansions_lower: Vec<InitialismPhrase> = Vec::new();
-        for expansion in expansions {
-            let expansion_lower: InitialismPhrase = expansion
-                .split(' ')
-                .map(|s| s.chars().map(|v| v.to_ascii_lowercase()).collect())
-                .collect();
-
-            expansions_lower.push(expansion_lower);
-        }
+        let expansions_lower = expansions
+            .iter()
+            .map(|expansion| {
+                expansion
+                    .split(' ')
+                    .map(|s| s.chars().map(|v| v.to_ascii_lowercase()).collect())
+                    .collect()
+            })
+            .collect();
 
         Self {
             expr: Box::new(Word::from_char_string(initialism.chars().collect())),
@@ -49,27 +49,25 @@ impl ExprLinter for InitialismLinter {
         let tok = matched_tokens.first()?;
         let source = tok.span.get_content(source);
 
-        let mut phrases = Vec::new();
-        for expansion_lower in &self.expansions_lower {
-            let mut expansion = expansion_lower.clone();
-            let first_letter = &mut expansion[0][0];
-
-            *first_letter = if source[0].is_ascii_uppercase() {
-                first_letter.to_ascii_uppercase()
-            } else {
-                first_letter.to_ascii_lowercase()
-            };
-
-            let phrase = expansion
-                .iter()
-                .flat_map(|word| std::iter::once(' ').chain(word.iter().copied()))
-                .skip(1)
-                .collect::<Vec<_>>();
-
-            phrases.push(phrase);
-        }
-
-        let suggestions = phrases.into_iter().map(Suggestion::ReplaceWith).collect();
+        let suggestions = self.expansions_lower
+            .iter()
+            .map(|expansion_lower| {
+                let mut expansion = expansion_lower.clone();
+                let first_letter = &mut expansion[0][0];
+                *first_letter = if source[0].is_ascii_uppercase() {
+                    first_letter.to_ascii_uppercase()
+                } else {
+                    first_letter.to_ascii_lowercase()
+                };
+                Suggestion::ReplaceWith(
+                    expansion
+                        .iter()
+                        .flat_map(|word| std::iter::once(' ').chain(word.iter().copied()))
+                        .skip(1)
+                        .collect::<Vec<_>>()
+                )
+            })
+            .collect::<Vec<_>>();
 
         Some(Lint {
             span: tok.span,
