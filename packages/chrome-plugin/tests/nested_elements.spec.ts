a07f9f5f315eec94a2183e8c583e41618ebe7728
Elijah Potter <me@elijahpotter.dev>
2026-01-13 13:39:46 -0700

    fix(chrome-ext): weird offset problems

--- packages/chrome-plugin/tests/nested_elements.spec.ts
+++ packages/chrome-plugin/tests/nested_elements.spec.ts
@@ -1,9 +1,5 @@
 import { test } from './fixtures';
-import {
-	assertHarperHighlightBoxes,
-	getTextarea,
-	replaceEditorContent,
-} from './testUtils';
+import { assertHarperHighlightBoxes, getTextarea, replaceEditorContent } from './testUtils';
 
 const TEST_PAGE_URL = 'http://localhost:8081/nested_elements.html';
 
@@ -18,17 +14,9 @@ test('Positions properly in oddly nested page.', async ({ page }, testInfo) => {
 
 	await page.waitForTimeout(6000);
 
-	if (testInfo.project.name == 'chromium') {
-		await assertHarperHighlightBoxes(page, [
-			{ x: 233.90625, y: 44, width: 48, height: 19 },
-			{ x: 281.90625, y: 44, width: 8, height: 19 },
-			{ x: 10, y: 61, width: 8, height: 19 },
-			{ x: 241.90625, y: 27, width: 24, height: 19 },
-		]);
-	} else {
-		await assertHarperHighlightBoxes(page, [
-			{ x: 10, y: 71, width: 57.599998474121094, height: 17 },
-			{ x: 218.8000030517578, y: 26, width: 21.600006103515625, height: 17 },
-		]);
-	}
+	await assertHarperHighlightBoxes(page, [
+		{ x: 396.390625, y: 243, width: 15.625, height: 19 },
+		{ x: 794.1875, y: 243, width: 23.421875, height: 19 },
+		{ x: 490, y: 260, width: 85.828125, height: 19 },
+	]);
 });
