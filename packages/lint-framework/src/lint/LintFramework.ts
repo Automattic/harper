80fc662c27a61712379273b10c9657b75e1b7873
loganbrinsmead <loganbrinsmead1115@gmail.com>
2026-01-14 14:07:29 -0800

    if suggestions check

--- packages/lint-framework/src/lint/LintFramework.ts
+++ packages/lint-framework/src/lint/LintFramework.ts
@@ -183,7 +183,7 @@ export default class LintFramework {
 
 						const previousBox = this.lastBoxes[closestIdx];
 						const suggestions = previousBox.lint.suggestions;
-						if (suggestions.length > 0) {
+						if (suggestions) {
 							previousBox.applySuggestion(suggestions[0]);
 						} else {
 							previousBox.ignoreLint?.();
