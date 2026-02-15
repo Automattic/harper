8a264cb2896ce600c823d42a7f39227e495ff1f5
86xsk <200443667+86xsk@users.noreply.github.com>
2026-02-14 20:24:03 -0600

    refactor(core): replace `ModalVerb::init` function
    Creates a `without_common_errors` counterpart to `with_common_errors`.

--- harper-core/src/patterns/modal_verb.rs
+++ harper-core/src/patterns/modal_verb.rs
@@ -2,24 +2,23 @@ use std::sync::LazyLock;
 
 use super::{Pattern, WordSet};
 
+static MODALS: [&str; 14] = [
+    "can", "can't", "could", "may", "might", "must", "shall", "shan't", "should", "will", "won't",
+    "would", "ought", "dare",
+];
+
 pub struct ModalVerb {
     inner: &'static WordSet,
 }
 
 impl Default for ModalVerb {
     fn default() -> Self {
-        let words = Self::init(false);
-        Self { inner: words }
+        Self::without_common_errors()
     }
 }
 
 impl ModalVerb {
-    fn init(include_common_errors: bool) -> &'static WordSet {
-        const MODALS: [&str; 14] = [
-            "can", "can't", "could", "may", "might", "must", "shall", "shan't", "should", "will",
-            "won't", "would", "ought", "dare",
-        ];
-
+    pub fn without_common_errors() -> Self {
         static CACHED_WITHOUT_COMMON_ERRORS: LazyLock<WordSet> = LazyLock::new(|| {
             let mut words = WordSet::new(&MODALS);
             MODALS.iter().for_each(|word| {
@@ -29,6 +28,12 @@ impl ModalVerb {
             words
         });
 
+        Self {
+            inner: &CACHED_WITHOUT_COMMON_ERRORS,
+        }
+    }
+
+    pub fn with_common_errors() -> Self {
         static CACHED_WITH_COMMON_ERRORS: LazyLock<WordSet> = LazyLock::new(|| {
             let mut words = WordSet::new(&MODALS);
             MODALS.iter().for_each(|word| {
@@ -39,16 +44,9 @@ impl ModalVerb {
             words
         });
 
-        if include_common_errors {
-            &CACHED_WITH_COMMON_ERRORS
-        } else {
-            &CACHED_WITHOUT_COMMON_ERRORS
+        Self {
+            inner: &CACHED_WITH_COMMON_ERRORS,
         }
-    }
-
-    pub fn with_common_errors() -> Self {
-        let words = Self::init(true);
-        Self { inner: words }
     }
 }
 
