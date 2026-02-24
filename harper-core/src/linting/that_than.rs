2afef21f6958cba0e4f48968c2e1b0485fc146ba
Elijah Potter <me@elijahpotter.dev>
2026-02-24 10:41:18 -0700

    chore(core): `just format`

--- harper-core/src/linting/that_than.rs
+++ harper-core/src/linting/that_than.rs
@@ -1,7 +1,7 @@
 use crate::linting::expr_linter::Chunk;
 use crate::{
-    Token, TokenKind,
-    expr::{Expr, SequenceExpr},
+    CharStringExt, Token, TokenKind,
+    expr::{Expr, FirstMatchOf, SequenceExpr},
     linting::{ExprLinter, Lint, LintKind, Suggestion},
 };
 
@@ -21,8 +21,21 @@ impl Default for ThatThan {
             .t_ws()
             .then_word_except(&["way"]);
 
+        let more_less_adj_that_you_think = SequenceExpr::word_set(&["more", "less"])
+            .t_ws()
+            .then_kind_either(TokenKind::is_adjective, TokenKind::is_adverb)
+            .t_ws()
+            .t_aco("that")
+            .t_ws()
+            .t_aco("you")
+            .t_ws()
+            .t_aco("think");
+
         Self {
-            expr: Box::new(adjective_er_that_nextword),
+            expr: Box::new(FirstMatchOf::new(vec![
+                Box::new(adjective_er_that_nextword),
+                Box::new(more_less_adj_that_you_think),
+            ])),
         }
     }
 }
@@ -35,11 +48,14 @@ impl ExprLinter for ThatThan {
     }
 
     fn match_to_lint(&self, toks: &[Token], src: &[char]) -> Option<Lint> {
-        if toks.len() != 5 {
-            return None;
-        }
+        let mut that_toks = toks
+            .iter()
+            .filter(|tok| tok.span.get_content(src).eq_ignore_ascii_case_str("that"));
 
-        let that_tok = &toks[2];
+        let that_tok = match (that_toks.next(), that_toks.next()) {
+            (Some(tok), None) => tok,
+            _ => return None,
+        };
 
         Some(Lint {
             span: that_tok.span,
@@ -202,6 +218,69 @@ mod tests {
     fn dont_flag_more_clear_that() {
         assert_lint_count(
             "Make it more clear that users need to download the VS tooling installer for .NET Core in VS.",
+            ThatThan::default(),
+            0,
+        );
+    }
+
+    #[test]
+    fn fix_more_complicated_that_you_think() {
+        assert_suggestion_result(
+            "Space Invaders is more complicated that you think.",
+            ThatThan::default(),
+            "Space Invaders is more complicated than you think.",
+        );
+    }
+
+    #[test]
+    fn dont_flag_more_likely_that_you_will() {
+        assert_lint_count(
+            "It is more likely that you will win this round.",
+            ThatThan::default(),
+            0,
+        );
+    }
+
+    #[test]
+    fn fix_less_obvious_that_you_think() {
+        assert_suggestion_result(
+            "It is less obvious that you think.",
+            ThatThan::default(),
+            "It is less obvious than you think.",
+        );
+    }
+
+    #[test]
+    fn fix_more_subtly_that_you_think() {
+        assert_suggestion_result(
+            "The issue is more subtly that you think.",
+            ThatThan::default(),
+            "The issue is more subtly than you think.",
+        );
+    }
+
+    #[test]
+    fn fix_more_complicated_that_you_think_uppercase() {
+        assert_suggestion_result(
+            "Space Invaders is more complicated THAT you think.",
+            ThatThan::default(),
+            "Space Invaders is more complicated THAN you think.",
+        );
+    }
+
+    #[test]
+    fn dont_flag_more_apparent_that_we_needed_logs() {
+        assert_lint_count(
+            "It became more apparent that we needed logs.",
+            ThatThan::default(),
+            0,
+        );
+    }
+
+    #[test]
+    fn dont_flag_less_likely_that_they_will_fail() {
+        assert_lint_count(
+            "It is less likely that they will fail.",
             ThatThan::default(),
             0,
         );
