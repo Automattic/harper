7947443c8d198cd539532385e4021058e8198b9e
86xsk <200443667+86xsk@users.noreply.github.com>
2026-04-18 13:34:24 -0500

    fix(core): `DictWordMetadata` equality and ordering
    Use `BTreeSet` instead of `Vec` as the underlying type for
    `dict_word_metadata::DerivedFrom`.
    
    This fixes Eq and Ord related implementations for `DictWordMetadata` by
    removing from consideration the order in which the words were inserted
    into `derived_from`.

--- harper-core/src/dict_word_metadata/derived_from.rs
+++ harper-core/src/dict_word_metadata/derived_from.rs
@@ -1,5 +1,7 @@
-use std::iter::Extend;
-use std::slice::Iter;
+use std::{
+    collections::{BTreeSet, btree_set},
+    iter::Extend,
+};
 
 use serde::{Deserialize, Serialize};
 
@@ -8,17 +10,15 @@ use crate::spell::CanonicalWordId;
 /// A container for storing word IDs that a word is considered to be derived from.
 #[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize, PartialOrd, Hash)]
 pub struct DerivedFrom {
-    inner: Vec<CanonicalWordId>,
+    inner: BTreeSet<CanonicalWordId>,
 }
 
 impl DerivedFrom {
-    /// Insert another word ID, if it's not already contained in the list.
+    /// Insert another word ID, if it's not already contained.
     ///
-    /// If it is already contained in the list, it's quietly ignored.
+    /// If it is already contained, it's quietly ignored.
     pub fn insert(&mut self, id: CanonicalWordId) {
-        if !self.contains(id) {
-            self.inner.push(id);
-        }
+        self.inner.insert(id);
     }
 
     /// Is the list empty? In other words, Does this word have no known words it's derived from?
@@ -34,40 +34,32 @@ impl DerivedFrom {
     /// Create a new `DerivedFrom` containing a single initial word ID.
     pub fn from_canonical_word_id(word_id: CanonicalWordId) -> Self {
         Self {
-            inner: vec![word_id],
+            inner: BTreeSet::from([word_id]),
         }
     }
 
     /// Get an iterator of the contained [`CanonicalWordId`].
-    pub fn iter(&self) -> Iter<'_, CanonicalWordId> {
+    pub fn iter(&self) -> btree_set::Iter<'_, CanonicalWordId> {
         self.inner.iter()
     }
 }
 
 impl Extend<CanonicalWordId> for DerivedFrom {
     fn extend<T: IntoIterator<Item = CanonicalWordId>>(&mut self, iter: T) {
-        // Extend additional word ID's, as long as they don't already exist.
-        // This is intended to emulate the behavior of a `HashSet`.
-        iter.into_iter().for_each(|canonical_word_id| {
-            self.insert(canonical_word_id);
-        });
+        self.inner.extend(iter);
     }
 }
 
 impl<'a> Extend<&'a CanonicalWordId> for DerivedFrom {
     fn extend<T: IntoIterator<Item = &'a CanonicalWordId>>(&mut self, iter: T) {
-        // Extend additional word ID's, as long as they don't already exist.
-        // This is intended to emulate the behavior of a `HashSet`.
-        iter.into_iter().copied().for_each(|canonical_word_id| {
-            self.insert(canonical_word_id);
-        });
+        self.inner.extend(iter);
     }
 }
 
 impl IntoIterator for DerivedFrom {
     type Item = CanonicalWordId;
 
-    type IntoIter = std::vec::IntoIter<Self::Item>;
+    type IntoIter = btree_set::IntoIter<Self::Item>;
 
     fn into_iter(self) -> Self::IntoIter {
         self.inner.into_iter()
