c9bd8df0352c9f1e0488181e0f5e058da3d0debf
Elijah Potter <me@elijahpotter.dev>
2026-04-28 09:52:08 -0600

    feat: get full rect

--- src-tauri/src/macos.rs
+++ src-tauri/src/macos.rs
@@ -1,8 +1,16 @@
 use accessibility::attribute::{AXAttribute, AXUIElementAttributes};
 use accessibility::ui_element::AXUIElement;
-use accessibility::{TreeVisitor, TreeWalker, TreeWalkerFlow};
+use accessibility::{Error, TreeVisitor, TreeWalker, TreeWalkerFlow};
 use core_foundation::base::{CFType, TCFType};
 use core_foundation::string::CFString;
+
+use accessibility_sys::{
+    AXValueGetValue, AXValueRef, kAXPositionAttribute, kAXSizeAttribute, kAXValueTypeCGPoint,
+    kAXValueTypeCGSize,
+};
+use cocoa::foundation::{NSPoint, NSSize};
+use core::ffi::c_void;
+use core::mem::MaybeUninit;
 
 pub fn main() {
     let el = AXUIElement::application(57046);
@@ -18,9 +26,7 @@ pub fn main() {
             {
                 dbg!(value);
 
-                let pos_attr =
-                    AXAttribute::<CFType>::new(&CFString::from_static_string("AXPosition"));
-                dbg!(element.attribute(&pos_attr));
+                dbg!(element_rect(&element));
             }
 
             TreeWalkerFlow::Continue
@@ -37,4 +43,46 @@ fn is_textarea(el: &AXUIElement) -> bool {
     }
 
     false
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct AxRect {
+    pub x: f64,
+    pub y: f64,
+    pub width: f64,
+    pub height: f64,
+}
+
+fn ax_value<T>(element: &AXUIElement, name: &str, value_type: u32) -> Result<Option<T>, Error> {
+    let attr = AXAttribute::<CFType>::new(&CFString::from_static_string(name));
+    let value = element.attribute(&attr)?;
+    let mut out = MaybeUninit::<T>::uninit();
+
+    let ok = unsafe {
+        AXValueGetValue(
+            value.as_CFTypeRef() as AXValueRef,
+            value_type,
+            out.as_mut_ptr() as *mut c_void,
+        )
+    };
+
+    Ok(ok.then(|| unsafe { out.assume_init() }))
+}
+
+pub fn element_rect(element: &AXUIElement) -> Result<Option<AxRect>, Error> {
+    let Some(position) = ax_value::<NSPoint>(element, kAXPositionAttribute, kAXValueTypeCGPoint)?
+    else {
+        return Ok(None);
+    };
+
+    let Some(size) = ax_value::<NSSize>(element, kAXSizeAttribute, kAXValueTypeCGSize)? else {
+        return Ok(None);
+    };
+
+    Ok(Some(AxRect {
+        x: position.x,
+        y: position.y,
+        width: size.width,
+        height: size.height,
+    }))
 }
