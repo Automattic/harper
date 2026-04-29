b523e2713ff197da8911b6c74be45e45786a62a4
Elijah Potter <me@elijahpotter.dev>
2026-04-29 10:50:24 -0600

    feat: set up clap with normal and highlighter branches

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
 use self::highlighter::Highlighter;
+use clap::{Parser, Subcommand};
 
 pub mod highlighter;
 pub mod rect;
@@ -8,22 +9,39 @@ use rect::Rect;
 #[cfg(target_os = "macos")]
 mod macos;
 
+#[derive(Parser)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Highlighter,
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    #[cfg(target_os = "macos")]
-    {
-        macos::main();
-    }
+    let args = Args::parse();
 
+    match args.command {
+        Some(Command::Highlighter) => run_highlighter(),
+        None => run_tauri(),
+    }
+}
+
+pub fn run_tauri() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+pub fn run_highlighter() {
     if let Err(error) = Highlighter::new().and_then(|mut h| {
         h.set_rects(vec![Rect::new(100., 100., 100., 100.)]);
         h.run_window_for_each_monitor()
     }) {
         eprintln!("failed to run highlighter: {error}");
     }
-
-    tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
 }
