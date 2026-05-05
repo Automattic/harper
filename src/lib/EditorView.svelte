1adf1198dbff052cfd71d19bc19f71d1699bb549
Jason Adams <jason.adams@automattic.com>
2026-05-05 15:21:26 -0600

    feat(settings): add settings window UI

---
+++ src/lib/EditorView.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { WorkerLinter } from "harper.js";
+  import { binaryInlined } from "harper.js/binaryInlined";
+  import { Editor } from "harper-editor";
+  import "harper-editor/style.css";
+
+  const linter = new WorkerLinter({ binary: binaryInlined });
+</script>
+
+<div class="h-screen w-screen">
+  <Editor {linter} />
+</div>
