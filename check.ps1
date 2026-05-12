63814d5fb94d6eda5788437ffde8a032c304625d
claw Guide  633 <1539489228@qq.com>
2026-05-12 12:29:25 +0800

    chore: remove debug script check.ps1

--- check.ps1
+++
@@ -1,7 +0,0 @@
-$content = Get-Content 'C:\Users\Administrator\.qclaw\workspace\harper-work\quick123-666\harper\harper-core\src\spell\mod.rs' -Raw -Encoding UTF8
-$old = 'let mut score = sug.edit_distance as i32 * 10;'
-if ($content -match [regex]::Escape($old)) {
-    Write-Host 'FOUND'
-} else {
-    Write-Host 'NOT FOUND'
-}
