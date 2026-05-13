cc6f4a0bb9697cd9c738322be9bf02a078ef74dd
cfcryptotrader-rgb <cfcryptotrader-rgb@users.noreply.github.com>
2026-05-13 05:50:18 -0300

    Refine way/wait auxiliary coverage

--- harper-core/src/linting/way_wait.rs
+++ harper-core/src/linting/way_wait.rs
@@ -1,41 +1,46 @@
-use crate::expr::{Expr, FirstMatchOf, SequenceExpr};
+use crate::expr::{Expr, SequenceExpr};
 use crate::linting::expr_linter::Chunk;
 use crate::linting::{ExprLinter, Lint, LintKind, Suggestion};
 use crate::{CharStringExt, Token};
 
 /// Corrects `way` when it is likely a typo for the verb `wait`.
 pub struct WayWait {
-    expr: FirstMatchOf,
+    expr: SequenceExpr,
 }
 
 impl Default for WayWait {
     fn default() -> Self {
-        let negative = SequenceExpr::word_set(&[
-            "can't", "cant", "cannot", "couldn't", "couldnt", "doesn't", "doesnt", "don't", "dont",
-            "won't", "wont",
+        let negative_auxiliary = SequenceExpr::any_of(vec![
+            Box::new(SequenceExpr::word_set(&[
+                "can't",
+                "cant",
+                "cannot",
+                "couldn't",
+                "couldnt",
+                "doesn't",
+                "doesnt",
+                "don't",
+                "dont",
+                "mightn't",
+                "mightnt",
+                "mustn't",
+                "mustnt",
+                "shan't",
+                "shant",
+                "shouldn't",
+                "shouldnt",
+                "won't",
+                "wont",
+            ])),
+            Box::new(SequenceExpr::fixed_phrase("can not")),
         ]);
 
-        let can_not = SequenceExpr::fixed_phrase("can not");
-
-        let following = SequenceExpr::word_set(&["for", "to", "until"]);
-
         Self {
-            expr: FirstMatchOf::new(vec![
-                Box::new(
-                    SequenceExpr::with(negative)
-                        .t_ws()
-                        .t_aco("way")
-                        .t_ws()
-                        .then(following),
-                ),
-                Box::new(
-                    SequenceExpr::with(can_not)
-                        .t_ws()
-                        .t_aco("way")
-                        .t_ws()
-                        .t_set(&["for", "to", "until"]),
-                ),
-            ]),
+            expr: SequenceExpr::with(negative_auxiliary)
+                .t_ws()
+                .t_aco("way")
+                .t_ws()
+                .t_set(&["for", "to", "until"]),
         }
     }
 }
@@ -78,18 +83,18 @@ mod tests {
     #[test]
     fn fix_cant_way_to() {
         assert_suggestion_result(
-            "Wow, incredible, can't way to try it.",
+            "Wow, just seeing this again, incredible, can't way to try it.",
             WayWait::default(),
-            "Wow, incredible, can't wait to try it.",
+            "Wow, just seeing this again, incredible, can't wait to try it.",
         );
     }
 
     #[test]
     fn fix_cannot_way_for() {
         assert_suggestion_result(
-            "If you cannot way for the next release, downgrade for now.",
+            "If it's a real bother and you cannot way for 2.10.16, go back to 2.10.12.",
             WayWait::default(),
-            "If you cannot wait for the next release, downgrade for now.",
+            "If it's a real bother and you cannot wait for 2.10.16, go back to 2.10.12.",
         );
     }
 
@@ -99,6 +104,34 @@ mod tests {
             "It doesn't way until completion though.",
             WayWait::default(),
             "It doesn't wait until completion though.",
+        );
+    }
+
+    #[test]
+    fn fix_can_not_way_for() {
+        assert_suggestion_result(
+            "I can not way for something better to be released.",
+            WayWait::default(),
+            "I can not wait for something better to be released.",
+        );
+    }
+
+    #[test]
+    fn fix_negative_modals() {
+        assert_suggestion_result(
+            "You shouldn't way to tell them.",
+            WayWait::default(),
+            "You shouldn't wait to tell them.",
+        );
+        assert_suggestion_result(
+            "We mustn't way until completion.",
+            WayWait::default(),
+            "We mustn't wait until completion.",
+        );
+        assert_suggestion_result(
+            "I shan't way for another release.",
+            WayWait::default(),
+            "I shan't wait for another release.",
         );
     }
 
