bec1e4944d9ea21b4956b0cc7b1f0c2d2aebbe97
hippietrail <hippytrail@gmail.com>
2026-05-19 00:35:45 +0800

    feat: use `RemoveSuffix` in a linter (`Damages`)

--- harper-core/src/linting/damages.rs
+++ harper-core/src/linting/damages.rs
@@ -148,10 +148,7 @@ impl ExprLinter for Damages {
         Some(Lint {
             span: damage_span,
             lint_kind: LintKind::Usage,
-            suggestions: vec![Suggestion::replace_with_match_case(
-                damage_chars[..6].to_vec(),
-                damage_chars,
-            )],
+            suggestions: vec![Suggestion::RemoveSuffix(1)],
             message: "Singular `damage` is correct when not referring to a court case.".to_string(),
             ..Default::default()
         })
