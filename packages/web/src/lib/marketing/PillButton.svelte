946d064ea16b7f321ada776c2c0cccb7943ecd12
Elijah Potter <me@elijahpotter.dev>
2026-05-20 15:17:04 -0600

    refactor(web): make previous changes to `PillButton` easier to read

--- packages/web/src/lib/marketing/PillButton.svelte
+++ packages/web/src/lib/marketing/PillButton.svelte
@@ -5,17 +5,24 @@ export let size: 'md' | 'lg' = 'md';
 export let disabled = false;
 export let label = '';
 
-$: classes = [
-	'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full font-bold no-underline transition duration-150 ease-out hover:-translate-y-px hover:no-underline',
-	size === 'lg' ? 'h-11 px-5 text-sm' : 'h-9 px-4 text-[0.82rem]',
-	kind === 'primary'
-		? 'border border-black/40 bg-black !text-white dark:border-white/40 dark:bg-white dark:!text-black'
-		: 'border border-black/10 bg-black/5 !text-black dark:border-white/15 dark:bg-white/10 dark:!text-white',
-	disabled ? 'cursor-default opacity-70 hover:translate-y-0' : '',
-	'[&_svg]:size-4 [&_svg]:shrink-0 [&_svg]:fill-current',
-]
-	.filter(Boolean)
-	.join(' ');
+const baseClasses =
+	'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full font-bold no-underline transition duration-150 ease-out hover:-translate-y-px hover:no-underline';
+const disabledClasses = 'cursor-default opacity-70 hover:translate-y-0';
+const iconClasses = '[&_svg]:size-4 [&_svg]:shrink-0 [&_svg]:fill-current';
+
+const sizes = {
+	md: 'h-9 px-4 text-[0.82rem]',
+	lg: 'h-11 px-5 text-sm',
+};
+
+const variants = {
+	primary:
+		'border border-black/40 bg-black !text-white dark:border-white/40 dark:bg-white dark:!text-black',
+	secondary:
+		'border border-black/10 bg-black/5 !text-black dark:border-white/15 dark:bg-white/10 dark:!text-white',
+};
+
+$: classes = `${baseClasses} ${sizes[size]} ${variants[kind]} ${disabled ? disabledClasses : ''} ${iconClasses}`;
 </script>
 
 {#if disabled}
