9946e8f4910b772f6409ff702ee6f751743ae1a5
mirsella <mirsella@protonmail.com>
2026-06-15 23:34:41 +0200

    feat: expose English isolation option

--- packages/harper.js/src/main.ts
+++ packages/harper.js/src/main.ts
@@ -82,4 +82,7 @@ export interface LintOptions {
 
 	/** Remove overlapping lints. An undefined value is assumed to be true. */
 	dedup?: boolean;
+
+	/** Ignore text that is unlikely to be English. An undefined value is assumed to be false. */
+	isolateEnglish?: boolean;
 }
