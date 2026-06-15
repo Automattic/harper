45475a08264a9d1cc3ce13eeba6512837f870858
Andrew Dunbar <hippytrail@gmail.com>
2026-06-16 03:29:41 +0800

    feat: the last days→the last few days (#3268)
    * feat: the last days→the last few days
    
    * fix: JSON format
    
    ---------
    
    Co-authored-by: Elijah Potter <me@elijahpotter.dev>

--- harper-core/src/linting/lint_group/mod.rs
+++ harper-core/src/linting/lint_group/mod.rs
@@ -232,6 +232,7 @@ use super::take_medicine::TakeMedicine;
 use super::that_than::ThatThan;
 use super::that_which::ThatWhich;
 use super::the_how_why::TheHowWhy;
+use super::the_last_days::TheLastDays;
 use super::the_my::TheMy;
 use super::the_point_for::ThePointFor;
 use super::the_proper_noun_possessive::TheProperNounPossessive;
@@ -795,6 +796,7 @@ impl LintGroup {
         insert_expr_rule!(ThatThan, true);
         insert_expr_rule!(ThatWhich, true);
         insert_expr_rule!(TheHowWhy, true);
+        insert_expr_rule!(TheLastDays, true);
         insert_expr_rule!(TheMy, true);
         insert_expr_rule!(ThePointFor, true);
         insert_expr_rule!(TheProperNounPossessive, true);
