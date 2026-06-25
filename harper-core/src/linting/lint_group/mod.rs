f631dc0a9b5f2db0bafb603859eb27ce9ce5be2e
Andrew Dunbar <hippytrail@gmail.com>
2026-06-25 18:50:30 +0800

    feat: jump a gun/jump the guns→jump the gun (#3404)
    * feat: jump a gun/jump the guns→jump the gun
    
    * fix: `.and_not()` was changed to `.but_not()`

--- harper-core/src/linting/lint_group/mod.rs
+++ harper-core/src/linting/lint_group/mod.rs
@@ -125,6 +125,7 @@ use super::its_contraction::ItsContraction;
 use super::its_possessive::ItsPossessive;
 use super::jealous_of::JealousOf;
 use super::johns_hopkins::JohnsHopkins;
+use super::jump_the_gun::JumpTheGun;
 use super::lead_rise_to::LeadRiseTo;
 use super::leaving_in_droves::LeavingInDroves;
 use super::left_right_hand::LeftRightHand;
@@ -697,6 +698,7 @@ impl LintGroup {
         insert_expr_rule!(ItsPossessive, true);
         insert_expr_rule!(JealousOf, true);
         insert_expr_rule!(JohnsHopkins, true);
+        insert_expr_rule!(JumpTheGun, true);
         insert_expr_rule!(LeadRiseTo, true);
         insert_expr_rule!(LeavingInDroves, true);
         insert_expr_rule!(LeftRightHand, true);
