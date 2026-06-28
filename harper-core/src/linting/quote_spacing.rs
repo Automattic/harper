89e51b2e1184e2fc5ec42bf9355c7afe376399f6
hippietrail <hippytrail@gmail.com>
2026-06-28 21:33:26 +0800

    feat: adds suggestions to `QuoteSpacing`

--- harper-core/src/linting/quote_spacing.rs
+++ harper-core/src/linting/quote_spacing.rs
@@ -26,17 +26,27 @@ impl Linter for QuoteSpacing {
     fn lint(&mut self, document: &Document) -> Vec<Lint> {
         let mut lints = Vec::new();
 
-        for m in self.expr.iter_matches_in_doc(document) {
+        for (index, m) in self.expr.iter_matches_in_doc(document).enumerate() {
             let matched_tokens = m.get_content(document.get_tokens());
 
             let Some(span) = matched_tokens.span() else {
                 continue;
             };
 
+            let mut suggestions = vec![
+                super::Suggestion::ReplaceWith(vec![' ', '"']),
+                super::Suggestion::ReplaceWith(vec!['"', ' ']),
+            ];
+
+            // Every odd-numbered quote is likely a close quote, ie space likely should be after it.
+            if index % 2 == 1 {
+                suggestions.swap(0, 1);
+            }
+
             lints.push(Lint {
-                span,
+                span: matched_tokens[1].span,
                 lint_kind: LintKind::Formatting,
-                suggestions: vec![],
+                suggestions,
                 message: "A quote must be preceded or succeeded by a space.".to_owned(),
                 priority: 31,
             })
@@ -53,7 +63,7 @@ impl Linter for QuoteSpacing {
 #[cfg(test)]
 mod tests {
     use super::QuoteSpacing;
-    use crate::linting::tests::{assert_lint_count, assert_no_lints};
+    use crate::linting::tests::{assert_lint_count, assert_no_lints, assert_suggestion_result};
 
     #[test]
     fn flags_missing_space_before_quote() {
@@ -77,5 +87,14 @@ mod tests {
     #[test]
     fn allows_quotes_at_end_of_sentence() {
         assert_no_lints("They shouted \"charge.\"", QuoteSpacing::default());
+    }
+
+    #[test]
+    fn fix_by_inserting_space_after() {
+        assert_suggestion_result(
+            "It's not a complete sentence since it lacks a verb but it would be valid as a title or answer to a question since \"dog runs\"are things which exist",
+            QuoteSpacing::default(),
+            "It's not a complete sentence since it lacks a verb but it would be valid as a title or answer to a question since \"dog runs\" are things which exist",
+        );
     }
 }
