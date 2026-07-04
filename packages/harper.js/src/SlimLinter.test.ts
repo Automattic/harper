892a67db570c0b0d8306d344f7123d22dead1c7c
Dronakurl <dronakurl@posteo.de>
2026-07-04 17:56:29 +0200

    fix: Handle Typst parser panic gracefully in WASM

--- packages/harper.js/src/SlimLinter.test.ts
+++ packages/harper.js/src/SlimLinter.test.ts
@@ -5,8 +5,9 @@ import LocalLinter from './LocalLinter';
 // We test the 'slim' binary separately from the main one because we utilize memoization.
 // To get the module to fail when requesting abilities is does not have, we need to load it in a separate context.
 
-test('Requesting a Typst parser with the slim binary should panic.', async () => {
+test('Requesting a Typst parser with the slim binary should return empty array.', async () => {
 	const linter = new LocalLinter({ binary: slimBinary });
 
-	await expect(linter.lint('', { language: 'typst' })).rejects.toThrowError();
+	const result = await linter.lint('', { language: 'typst' });
+	expect(result).toEqual([]);
 });
