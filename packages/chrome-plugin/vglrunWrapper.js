bff7735f503d2a1395bde07b69f267e645cc10e8
Elijah Potter <me@elijahpotter.dev>
2026-07-09 13:44:56 -0600

    fix(chrome-ext): case where it is not installed

--- packages/chrome-plugin/vglrunWrapper.js
+++ packages/chrome-plugin/vglrunWrapper.js
@@ -6,7 +6,19 @@ import { chromium } from 'playwright';
 const chromiumExecutablePath = chromium.executablePath();
 const args = process.argv.slice(2).join(' ');
 
-spawnSync(`vglrun -d :0 -- ${chromiumExecutablePath} ${args}`, {
-	stdio: [0, 1, 2, 3, 4],
-	shell: true,
-});
+function hasProgram(program) {
+  const result = spawnSync(program, ['--version'], { stdio: 'ignore' });
+  return result.error?.code !== 'ENOENT';
+}
+
+if (hasProgram("vglrun")){
+  spawnSync(`vglrun -d :0 -- ${chromiumExecutablePath} ${args}`, {
+  	stdio: [0, 1, 2, 3, 4],
+  	shell: true,
+  });
+}else{
+   spawnSync(`${chromiumExecutablePath} ${args}`, {
+  	stdio: [0, 1, 2, 3, 4],
+  	shell: true,
+  });
+}
