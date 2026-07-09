094913929151cde856178b3217e2f8b91fd3e6ea
Elijah Potter <me@elijahpotter.dev>
2026-07-09 14:17:11 -0600

    fix: AAAAAAAAA

--- packages/chrome-plugin/vglrunWrapper.js
+++ packages/chrome-plugin/vglrunWrapper.js
@@ -7,18 +7,18 @@ const chromiumExecutablePath = chromium.executablePath();
 const args = process.argv.slice(2).join(' ');
 
 function hasProgram(program) {
-  const result = spawnSync(program, ['--version'], { stdio: 'ignore' });
-  return result.error?.code !== 'ENOENT';
+	const result = spawnSync(program, ['--version'], { stdio: 'ignore' });
+	return result.error?.code !== 'ENOENT';
 }
 
-if (hasProgram("vglrun")){
-  spawnSync(`vglrun -d :0 -- ${chromiumExecutablePath} ${args}`, {
-  	stdio: [0, 1, 2, 3, 4],
-  	shell: true,
-  });
-}else{
-   spawnSync(`${chromiumExecutablePath} ${args}`, {
-  	stdio: [0, 1, 2, 3, 4],
-  	shell: true,
-  });
+if (hasProgram('vglrun')) {
+	spawnSync(`vglrun -d :0 -- ${chromiumExecutablePath} ${args}`, {
+		stdio: [0, 1, 2, 3, 4],
+		shell: true,
+	});
+} else {
+	spawnSync(`${chromiumExecutablePath} ${args}`, {
+		stdio: [0, 1, 2, 3, 4],
+		shell: true,
+	});
 }
