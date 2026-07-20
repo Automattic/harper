dfc72042d7331c5aa550e3f54364ad5ede8dcbde
Elijah Potter <me@elijahpotter.dev>
2026-07-20 10:14:40 -0600

    refactor(web): into reusable page

--- packages/web/src/routes/admin/problematic-lints/[slug]/+page.svelte
+++ packages/web/src/routes/admin/problematic-lints/[slug]/+page.svelte
@@ -1,40 +1,14 @@
 <script lang="ts">
-import { GutterCenter, Isolate } from 'components';
-import BarChart from '$lib/components/BarChart.svelte';
 import type { PageProps } from './$types';
+import AnalyticsPage from '$lib/components/AnalyticsPage.svelte';
 
 let { data }: PageProps = $props();
-let counts = $derived(data.counts as Record<string, number>);
-let entries = $derived(Object.entries(counts).toSorted(([_a, a], [_b, b]) => b - a));
+let links = {
+  All: "/admin/problematic-lints/all",
+  ["Last 30 Days"]: "/admin/problematic-lints/last30days",
+  ["Last Day"]: "/admin/problematic-lints/lastday",
+};
+
 </script>
 
-<Isolate>
-  <h1>Most Reported Erroring Lint IDs</h1>
-
-  <div class="flex flex-row [&>a]:px-4">
-    <a href="/admin/problematic-lints/all">All</a>
-    <a href="/admin/problematic-lints/last30days">Last 30 Days</a>
-    <a href="/admin/problematic-lints/lastday">Last Day</a>
-  </div>
-
-  <GutterCenter >
-    <BarChart data={counts} title="Most Commonly Complained-About Lint IDs"/>
-    
-    <table>
-    	<thead>
-    		<tr>
-    			<th>Lint ID</th>
-    			<th>Value</th>
-    		</tr>
-    	</thead>
-    	<tbody>
-        {#each entries as [key, value] }
-    		  <tr>
-    		  	<th scope="row">{key}</th>
-    		  	<td>{value}</td>
-    		  </tr>
-        {/each}
-    	</tbody>
-    </table>
-  </GutterCenter>
-</Isolate>
+<AnalyticsPage data={data} title="Most Reported Erroring Lint IDs" {links}/>
