afa6d7b2131cb1e6c708d4bda2d1d989cb679f46
Elijah Potter <me@elijahpotter.dev>
2026-07-28 07:55:07 -0600

    docs(core): document the main items

--- harper-core/src/linting/pooled_linter/mod.rs
+++ harper-core/src/linting/pooled_linter/mod.rs
@@ -14,6 +14,19 @@ type SelectedPool<T> = multi_thread_pool::MultiThreadPool<T>;
 #[cfg(not(feature = "concurrent"))]
 type SelectedPool<T> = SingleThreadPool<T>;
 
+/// A `PooledLinter` is a data structure that wraps around a [`Linter`] constructor.
+///
+/// When the `concurrent` feature is enabled, it uses the provided constructor function to create a
+/// pool of child linters. This pool exists so a thread pool can perform linting operations
+/// without needing to repeatedly construct or otherwise manage linters. When a linter is needed, an
+/// unlocked linter from the pool is borrowed, the linting is performed, then it is released back to
+/// the pool. If there are no available linters in the pool, one is added. The pool will rarely, if
+/// ever, have more than N elements in it, where N is the number of CPUs available to the program.
+///
+///
+/// When the `concurrent` feature is _disabled_, no pool is instantiated. Instead, a new linter
+/// will be constructed for each call. This is understandably quite expensive, so if you intend to
+/// use this data structure, it's highly recommended that you enable `concurrent`
 pub struct PooledLinter<L: Linter> {
     pool: SelectedPool<L>,
     description: String,
@@ -53,6 +66,8 @@ impl<L: Linter> Clone for PooledLinter<L> {
 
 #[cfg(test)]
 pub mod for_tests {
+    /// Constructs a static `PooledLinter` for use in tests. Provide the name of the linter under
+    /// test, its type, and an expression to construct it.
     macro_rules! create_test_pool {
         ($linter_name:ident, $linter_ty:ty, $linter_ctor:expr) => {
             mod test_group_container {
