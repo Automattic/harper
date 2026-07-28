ed42bdcefbd7a7b0986e5476160b05a2a47a982a
Elijah Potter <me@elijahpotter.dev>
2026-07-28 14:01:51 -0600

    fix(core): issues from the merge

--- harper-core/src/linting/lint_group/mod.rs
+++ harper-core/src/linting/lint_group/mod.rs
@@ -1182,19 +1182,19 @@ mod tests {
 
     #[test]
     fn fix_stoped() {
-        assert_suggestion_result("He stoped to think.", test_group(), "He stopped to think.");
+        assert_suggestion_result("He stoped to think.", test_linter(), "He stopped to think.");
     }
 
     #[test]
     fn allow_playthrough() {
-        assert_no_lints("I recently did a playthrough.", test_group());
+        assert_no_lints("I recently did a playthrough.", test_linter());
     }
 
     #[test]
     fn fixes_politicans() {
         assert_suggestion_result(
             "I guess politicans like to complain.",
-            test_group(),
+            test_linter(),
             "I guess politicans like to complain.",
         );
     }
@@ -1203,7 +1203,7 @@ mod tests {
     fn fixes_benefitting() {
         assert_suggestion_result(
             "Who is really benefitting from it?",
-            test_group(),
+            test_linter(),
             "Who is really benefiting from it?",
         );
     }
@@ -1212,7 +1212,7 @@ mod tests {
     fn fixes_easir() {
         assert_suggestion_result(
             "It makes it easir to select it.",
-            test_group(),
+            test_linter(),
             "It makes it easier to select it.",
         );
     }
@@ -1221,7 +1221,7 @@ mod tests {
     fn fixes_buget() {
         assert_suggestion_result(
             "It was in their buget range.",
-            test_group(),
+            test_linter(),
             "It was in their budget range.",
         );
     }
