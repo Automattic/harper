e9740aab8ed43a614808dc77a63a77ee59a0ff25
Elijah Potter <me@elijahpotter.dev>
2026-07-28 11:38:06 -0600

    fix: appease Biome

--- packages/web/src/lib/components/AnalyticsPage.svelte
+++ packages/web/src/lib/components/AnalyticsPage.svelte
@@ -10,7 +10,7 @@ type Props = {
 
 let { data, title, links }: Props = $props();
 
-let counts = $derived(data.counts);
+let counts = $derived(data);
 let entries = $derived(Object.entries(counts).toSorted(([_a, a], [_b, b]) => b - a));
 </script>
 
