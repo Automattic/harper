db745f04eb790bd3ac8062a02db770a3c96f3d86
86xsk <200443667+86xsk@users.noreply.github.com>
2026-08-08 15:15:40 -0500

    refactor(core): remove redundant check
    We already check if `out` is empty above. As such, the only case where
    this could be false is if
    `out.end < out.start`
    which would be an unsupported use of Span.

--- harper-core/src/expr/sequence_expr.rs
+++ harper-core/src/expr/sequence_expr.rs
@@ -87,11 +87,8 @@ impl Expr for SequenceExpr {
                 continue;
             }
 
-            // Only expand the window if the match actually covers some tokens
-            if out.end > out.start {
-                window.expand_to_include(out.start);
-                window.expand_to_include(out.end.checked_sub(1).unwrap_or(out.start));
-            }
+            window.expand_to_include(out.start);
+            window.expand_to_include(out.end.checked_sub(1).unwrap_or(out.start));
         }
 
         Some(window)
