162ed00a930ee4ae7e0863bb10f7f3fd1f6595d5
hippietrail <hippytrail@gmail.com>
2026-08-16 23:13:06 +0700

    feat: off-limit→off-limits

--- harper-core/src/linting/lint_group/mod.rs
+++ harper-core/src/linting/lint_group/mod.rs
@@ -183,6 +183,7 @@ use super::number_suffix_capitalization::NumberSuffixCapitalization;
 use super::numeric_range_en_dash::NumericRangeEnDash;
 use super::obsess_preposition::ObsessPreposition;
 use super::of_course::OfCourse;
+use super::off_limits::OffLimits;
 use super::oldest_in_the_book::OldestInTheBook;
 use super::on_floor::OnFloor;
 use super::once_or_twice::OnceOrTwice;
@@ -773,6 +774,7 @@ impl LintGroup {
         insert_expr_rule!(NumericRangeEnDash);
         insert_expr_rule!(ObsessPreposition);
         insert_expr_rule!(OfCourse);
+        insert_expr_rule!(OffLimits);
         insert_expr_rule!(OldestInTheBook);
         insert_expr_rule!(OnFloor);
         insert_expr_rule!(OnceOrTwice);
