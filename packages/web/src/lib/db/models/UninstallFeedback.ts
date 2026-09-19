913f95518beca39cbe70d9cbb3fb35c9f55ece3c
Elijah Potter <me@elijahpotter.dev>
2026-09-19 15:16:20 -0600

    feat(web): add tools for querying between periods

--- packages/web/src/lib/db/models/UninstallFeedback.ts
+++ packages/web/src/lib/db/models/UninstallFeedback.ts
@@ -1,4 +1,4 @@
-import { gte } from 'drizzle-orm';
+import { gte, lte, and } from 'drizzle-orm';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { db } from '..';
 import { uninstallFeedbackTable } from '../schema';
@@ -24,5 +24,12 @@ export default class UninstallFeedback {
 			.select()
 			.from(uninstallFeedbackTable)
 			.where(gte(uninstallFeedbackTable.timestamp, date));
+	}
+
+	public static async getAllBetween(start: Date, end: Date) {
+		return await db
+			.select()
+			.from(uninstallFeedbackTable)
+			.where(and( gte(uninstallFeedbackTable.timestamp, start), lte(uninstallFeedbackTable.timestamp ,end)));
 	}
 }
